f20cd3f66ddefdcc506728fb3b204ebd62ed535b
abhaysahu10 <48154295+abhaysahu10@users.noreply.github.com>
2021-08-22 22:07:12 +0530

    included smaCrossover function to smaStrategy

--- 3_Implementation/src/sma.c
+++ 3_Implementation/src/sma.c
@@ -1,15 +1,15 @@
 #include <header.h>
 
-float sma(int days, int crtday, float *coloumnArray)// days: avg of last n days, crtday: current day, price: array of coloumnArray till date
+float sma(int days, int crtday, float *columnArray)// days: avg of last n days, crtday: current day, price: array of coloumnArray till date
 {
     float sum = 0; // sum of all days price
-    if(crtday<=(coloumnArray[0]-days)){
+    if(crtday<=(columnArray[0]-days)){
         for (int i = crtday; i<(crtday+days); i++){
-            sum += coloumnArray[i];
+            sum += columnArray[i];
         }
     }
     else{
         return -1;
     }
-    return (sum/days); //returning Simple moving average
+    return ((days-sum)/days); //returning Simple moving average
 }
