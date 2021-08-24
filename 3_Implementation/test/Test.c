e6eb31aac5776b22d7608eefabf5d1769c2016ac
Alna <alnasatheesh23@gmail.com>
2021-08-24 17:23:19 +0530

    test for willR updated

--- 3_Implementation/test/Test.c
+++ 3_Implementation/test/Test.c
@@ -1,6 +1,7 @@
 #include <unity.h>
 #include <unity_internals.h>
 #include "header.h"
+#include "WillR.h"
 
 #define PROJECT_NAME "TradingStrategy"
 
@@ -11,21 +12,33 @@ void tearDown(){}
 
 void testSma(void);
 void testBollingerBands(void);
+void testWilliamR(void);
 
 //array to test sma and crossover functions
-float *close;
+float *close,*low,*high;
 //array to test sma and crossover functions
-const char *filePath = "LTTS.csv";
+ char *filePath = "LTTS.csv";
+int length;
 int main(){
 
     //array to test sma and crossover functions
-    const char *filePath = "LTTS.csv";
+    length = findLength(filePath);
+    //const char *filePath = "LTTS.csv";
     FILE *file = fopen(filePath,"r");
+    float *coloumn2 = readColumn(file,3);
+       high = malloc(length*sizeof(float));
+        memcpy(high,coloumn2,length*sizeof(float));
+
+        
+        float *coloumn3 = readColumn(file,4);
+        low = malloc(length*sizeof(float));
+        memcpy(low,coloumn3,length*sizeof(float));
     close = readColumn(file,7);
     UNITY_BEGIN();
 
     RUN_TEST(testSma);
     RUN_TEST(testBollingerBands);
+    RUN_TEST(testWilliamR);
 
     return UNITY_END();
 }
@@ -44,4 +57,17 @@ void testBollingerBands(void)
     TEST_ASSERT_EQUAL(1,lowerBand(2,20,365,close));
     TEST_ASSERT_EQUAL(-1,movingMiddleBand(20,365,close));
     TEST_ASSERT_EQUAL(-1,stdDev(20,365,close));
+}
+
+void testWilliamR(void)
+{
+        //TEST_ASSERT_EQUAL(0,WilliamR(filePath));
+        int length = findLength(filePath);
+        TEST_ASSERT_FLOAT_WITHIN(-20.00,20.00,findWillR(100,14,length,10,close , high,low));
+        TEST_ASSERT_FLOAT_WITHIN(1000.0,3000,findDMA(100,10,close,length));
+}
+
+void testFindLength(void)
+{
+    TEST_ASSERT_TRUE(findLength(filePath)) ;
 }
