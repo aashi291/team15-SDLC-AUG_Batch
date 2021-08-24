db7c6f40292b442c433443de91e75faf256e84f7
The-lana <82940429+The-lana@users.noreply.github.com>
2021-08-24 14:28:16 +0530

    Update WillR.c

--- 3_Implementation/src/WillR.c
+++ 3_Implementation/src/WillR.c
@@ -182,31 +182,3 @@ bool buyCondition(int DMA100, int WillR, float *close, int i)
 return NULL;
 }
 
-int findLength(char *filePath)
-{
-    int count = 0;  // Line counter (result)
-    
-    char c;  // To store a character read from file
-  
-    // Open the file
-    FILE *fp = fopen(filePath, "r");
-  
-    // Check if file exists
-    if (fp == NULL)
-    {
-        printf("Could not open file %s", filePath );
-        return 0;
-    }
-  
-    // Extract characters from file and store in character c
-    for (c = getc(fp); c != EOF; c = getc(fp))
-        if (c == '\n') // Increment count if this character is newline
-            count = count + 1;
-  
-    
-    fclose(fp);     // Close the file
-  
-  
-    return count-1;
-}
-
